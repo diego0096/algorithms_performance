f1f8ab137c520e178a91c9c6321c42c1a9baea20
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-03-29 22:43:53 -0500

    performance

--- performance/time/performance-now.js
+++ performance/time/performance-now.js
@@ -1,4 +1,4 @@
-/* const performance = require('perf_hooks');
+const performance = require('perf_hooks');
 
 function contar(n) {
     for(let i = 0; i < n; i++) {
@@ -12,17 +12,17 @@ let final_tiempo = performance.performance.now();
 let duracion = final_tiempo - inicio_tiempo;
 
 console.log(`El algoritmo constar ha durado ${duracion}ms.`)
- */
 
-function contar(n) {
+
+function count(n) {
     for(let i = 0; i < n; i++) {
         console.log(i);
     }
 }
 
-let inicio_tiempo = performance.now();
-contar(5)
-let final_tiempo = performance.now();
-let duracion = final_tiempo - inicio_tiempo;
+let timeInit = performance.now();
+count(5)
+let timeEnd = performance.now();
+let during = timeEnd - timeInit;
 
-console.log(`El algoritmo constar ha durado ${duracion}ms.`)
+console.log(`El algoritmo constar ha durado ${during}ms.`)
