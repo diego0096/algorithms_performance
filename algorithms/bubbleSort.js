383a617ba90bb8c1d05acd863625c9b9d48071c1
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-04-17 19:33:34 -0500

    algorithms

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,14 +1,14 @@
 /**
  * Complejidad Temporal -> O( n^2 )
- * Complejidad Espacial -> O(  )
- * Espacio Auxiliar -> O(  )
+ * Complejidad Espacial -> O( n )
+ * Espacio Auxiliar -> O( 1 )
  */
-function bubbleSort(arreglo) {
-  let longitud = arreglo.length; // O(1)
-  for (let i = 0; i < longitud; i++) { // O(n)
-    for (let j = 0; j < longitud; j++) { // O(n)
+function bubbleSort(arreglo) { // O(n)
+  let longitud = arreglo.length; // O(1) O(1)
+  for (let i = 0; i < longitud; i++) { // O(n) O(1)
+    for (let j = 0; j < longitud; j++) { // O(n) O(1)
       if (arreglo[j] > arreglo[j + 1]) { // O(1)
-        let temporal = arreglo[j]; // O(1)
+        let temporal = arreglo[j]; // O(1) O(1)
         arreglo[j] = arreglo[j + 1]; // O(1)
         arreglo[j + 1] = temporal; // O(1)
       }
