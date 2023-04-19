63d7f2abe6bdaae46b038625b19c5ca203e5fd3b
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-04-19 12:21:10 -0500

    algorithms

---
+++ algorithms/selectionSort.js
@@ -0,0 +1,23 @@
+/**
+ * Complejidad Temporal -> O( n^2 )
+ * Complejidad Espacial -> O( n )
+ * Espacio Auxiliar -> O( 1 )
+ */
+function selectionSort(arreglo) {  // O(n)
+  let longitud = arreglo.length; // O(1) O(1)
+
+  for (let i = 0; i < longitud; i++) { // O(n) O(1)
+    let minimo = i; // O(1) O(1)
+    for (let j = i + 1; j < longitud; j++) { // O(n) O(1)
+      if (arreglo[j] < arreglo[minimo]) { // O(1)
+        minimo = j; // O(1)
+      }
+    }
+    if (minimo != i) { // O(1)
+      let temporal = arreglo[i]; // O(1) O(1)
+      arreglo[i] = arreglo[minimo]; // O(1)
+      arreglo[minimo] = temporal; // O(1)
+    }
+  }
+  return arreglo; // O(1)
+}
